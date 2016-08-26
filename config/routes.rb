b4a33a254e60c645f9418a5b25c0bc12bb1be0c0
siewyuk <cpekwah@gmail.com>
2016-08-26 17:58:36 +0800

    Added conversations and messages controllers

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,11 @@ Rails.application.routes.draw do
   resources :users, controller: "users", only: :show
   get "/auth/:provider/callback" => "sessions#create_from_omniauth"
 
+  resources : conversations, only: [:index, :create] do
+    resources :messages, only: [:index, :create]
+  end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -84,4 +89,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+
 end
