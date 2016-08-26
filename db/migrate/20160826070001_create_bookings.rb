aed3828bfb952510855ec83960319f0a227f363e
yrlow <yrlow061208>
2016-08-26 15:04:52 +0800

    user clerance created but no fb link yet. also changed the sequence of the migration files.

--- db/migrate/20160826070001_create_bookings.rb
+++ db/migrate/20160826070001_create_bookings.rb
@@ -2,7 +2,7 @@ class CreateBookings < ActiveRecord::Migration
   def change
     create_table :bookings do |t|
       t.datetime :date
-      t.boolean :private, :null => false, :default => 0 
+      t.boolean :private, :null => false, :default => false 
       t.string :title
       t.text :notes
       t.references :facility, index: true, foreign_key: true
